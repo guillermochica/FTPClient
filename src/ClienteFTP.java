f826376c72693bc6e4ebcfab573d195d65e8b691
guillermochica <1bgchica@gmail.com>
2017-04-05 19:35:35 +0200

    Add active mode

--- src/ClienteFTP.java
+++ src/ClienteFTP.java
@@ -7,97 +7,116 @@ import java.io.PrintWriter;
 import java.net.*;
 
 public class ClienteFTP {
-	
-	private UsuarioFTP user; //Usuario FTP con el que me voy a conectar
-	boolean modo; //true activo, false pasivo
-	
-	//Atributos de socket
-	Socket s;
-	Socket sData;
-	ServerSocket sActive;
+
+	private UsuarioFTP user; // Usuario FTP con el que me voy a conectar
+	boolean modo; // true activo, false pasivo
+	boolean conectado;
+
+	// Atributos de socket
+	Socket s; // socket de comandos
+	Socket sData; // socket de datos
+	ServerSocket sActive; // socket de modo activo
 	BufferedReader r;
-	PrintWriter p;
-	PrintWriter pData;
-	
-	
-	int dataPort;
+	PrintWriter p; // para enviar comandos
+	PrintWriter pData; // para enviar datos
+
+	int dataPort; // puerto destino para modo pasivo
 	InetAddress ip;
-	
-	
-	public ClienteFTP(UsuarioFTP u, boolean m) throws IOException { 
+
+	public ClienteFTP(UsuarioFTP u, boolean m) throws IOException {
 		user = u;
 		modo = m;
+		conectado = false;
 	}
-	
+
 	public void conectar(InetAddress ip, int port) throws IOException {
-		
+
 		this.ip = ip;
 		s = new Socket(ip, port);
 		r = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		p = new PrintWriter(s.getOutputStream());
-		
-		System.out.println(r.readLine());
-		
-		//Hace login
-		try{
-			log(user.getUser(), user.getPass());
-			
 
-		}catch(LoginException e) {
+		System.out.println(r.readLine());
+
+		// Hace login
+
+		try {
+			log(user.getUser(), user.getPass());
+
+		} catch (LoginException e) {
 			System.out.println(e);
 		}
-		if(modo) {
-			conectarActivo(ip,port);
-		}
-		else{
+
+		if (modo) {
+			conectarActivo(ip, port);
+		} else {
 			this.conectarPasv(ip, port);
 		}
-		
+
 	}
-	
-	
+
 	public void desconectar() throws IOException {
 		p.println("QUIT");
 		p.flush();
 		System.out.println(r.readLine());
+		s.close();
+
 	}
-	
-	
+
 	public void subirFichero(String fichero, InetAddress ip) throws IOException {
-		
-		
-		File file = new File(fichero);
-		BufferedReader reader = null;
-		try{
-		 reader = new BufferedReader(new FileReader(file));
-		}catch(IOException e){
-			System.out.println("No existe el archivo "+fichero);
-		}
-		
-		if(reader!=null){
+
+		if (modo) { // modo activo
 			p.println("STOR " + fichero);
 			p.flush();
-
-			
 			System.out.println(r.readLine());
+
+			this.sActive = new ServerSocket(10 * 256 + 1);
+			System.out.println("Esperando conexion en el puerto " + 10 * 256 + 1);
+			sData = sActive.accept();
+			System.out.println("Conectado servidor");
+			pData = new PrintWriter(sData.getOutputStream());
+
+		} else { // modo pasivo
+
+			sData = new Socket(ip, dataPort);
+
+			pData = new PrintWriter(sData.getOutputStream());
+
+		}
+
+		File file = new File(fichero);
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+		} catch (IOException e) {
+			System.out.println("No existe el archivo " + fichero);
+		}
+
+		if (reader != null) {
+			if (!this.modo) { // si estamos en modo activo ya se ha enviado este
+								// comando
+				p.println("STOR " + fichero);
+				p.flush();
+				System.out.println(r.readLine());
+			}
 
 			String text = null;
 			while ((text = reader.readLine()) != null) {
-				
+
 				pData.println(text);
 				pData.flush();
 			}
 
 			reader.close();
 			sData.close();
+
 			System.out.println(r.readLine());
 		}
 
 	}
-	
+
 	public void log(String user, String pass) throws LoginException, IOException {
-		
-		
+
 		// Envia login
 		p.println("USER " + user);
 		p.flush();
@@ -110,77 +129,55 @@ public class ClienteFTP {
 		p.flush();
 		String respuesta = r.readLine();
 		System.out.println(respuesta);
-		
 
 		if (respuesta.equals("530 Login or password incorrect!")) {
 			throw new LoginException(user, pass);
 		} else {
 			this.user.setLogged(true);
-			
+
 		}
 	}
-	
-	public void conectarPasv(InetAddress ip, int port) throws IOException {
-		
-		
-		
-		
-		if(this.user.getLogged()) {
+
+	private void conectarPasv(InetAddress ip, int port) throws IOException {
+
+		if (this.user.getLogged()) {
 			System.out.println("Inicio conexion pasiva");
-			
+
 			p.println("TYPE I");
 			p.flush();
 			System.out.println(r.readLine());
-			
+
 			p.println("PASV");
 			p.flush();
 			String resp = r.readLine();
 			System.out.println(resp);
 			String a[] = resp.split(",");
-			dataPort = Integer.parseInt(a[4])*256 + Integer.parseInt(a[5].replace(")", ""));
+			dataPort = Integer.parseInt(a[4]) * 256 + Integer.parseInt(a[5].replace(")", ""));
 
-			System.out.println("Data port: "+dataPort);
-			
-			sData = new Socket(ip, dataPort);
-			
-			pData = new PrintWriter(sData.getOutputStream());
-			
+			System.out.println("Data port: " + dataPort);
+
 		}
 
 	}
-	
-	public void conectarActivo(InetAddress ip, int port) throws IOException{
-		
-		
-		
-		
-		if(this.user.getLogged()) {
+
+	private void conectarActivo(InetAddress ip, int port) throws IOException {
+
+		if (this.user.getLogged()) {
 			System.out.println("Inicio conexion activa");
 			p.println("TYPE I");
 			p.flush();
 			System.out.println(r.readLine());
 
-			//Comando port en la forma: PORT d1,d2,d3,d4,d5,d6
-			//los cuatro primeros digitos son los digitos de la dir ip cliente
-			//los dos ultimos valores para obtener el puerto de datos cliente
-			//puerto de datos cliente= d5*256 + d6. > que 1024
-			
+			// Comando port en la forma: PORT d1,d2,d3,d4,d5,d6
+			// los cuatro primeros digitos son los digitos de la dir ip cliente
+			// los dos ultimos valores para obtener el puerto de datos cliente
+			// puerto de datos cliente= d5*256 + d6. > que 1024
 
 			p.println("PORT 192,168,4,2,10,1");
 			p.flush();
-			System.out.println( r.readLine());
-			
-			p.println("RETR puertoFZServer.txt");
-			p.flush();
-			System.out.println( r.readLine());
-			
-			this.sActive = new ServerSocket(10*256+1);
-			System.out.println("Esperando conexion en el puerto "+10*256+1);
-			sData = sActive.accept();
-			pData = new PrintWriter(sData.getOutputStream());
-			
+			System.out.println(r.readLine());
 
 		}
 	}
-	
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -7,46 +7,41 @@ import java.net.UnknownHostException;
 public class Principal {
 
 	public static void main(String[] args) throws IOException {
-		Scanner s=new Scanner(System.in);
-		
+		Scanner s = new Scanner(System.in);
+
 		System.out.print("Ingrese usuario: ");
 		String user = s.next();
 		System.out.print("Ingrese contraseña: ");
 		String pass = s.next();
 		System.out.print("Escriba p para modo pasivo o a para activo: ");
 		String m = s.next();
-		
-		boolean modo=false;
-		
-		if(m.equals("p")) {
-			modo=false;
-		}
-		else if(m.equals("a")){
-			modo=true;
-		}
-		else{
+
+		boolean modo = false;
+
+		if (m.equals("p")) {
+			modo = false;
+		} else if (m.equals("a")) {
+			modo = true;
+		} else {
 			System.out.println("Error: conectando en modo pasivo por defecto");
-			modo=false;
+			modo = false;
 		}
 
 		UsuarioFTP u = new UsuarioFTP(user, pass);
-		
+
 		InetAddress ip = InetAddress.getLocalHost();
-		ClienteFTP c = new ClienteFTP(u,modo);
-		
+		ClienteFTP c = new ClienteFTP(u, modo);
+
 		c.conectar(ip, 21);
-		
-		//Si nos hemos logueado con exito
-		if(u.getLogged()) {
+
+		// Si nos hemos logueado con exito
+		if (u.getLogged()) {
 			System.out.print("Ingrese el nombre del fichero (p. ej.: paraEnviar.txt): ");
 			String fichero = s.next();
 			c.subirFichero(fichero, ip);
-			c.desconectar();
 		}
-		
-		
-		
-		
+
+		c.desconectar();
 
 	}
 
